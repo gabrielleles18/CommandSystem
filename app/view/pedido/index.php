e5332520a96e53e616db5e4d3777ea43469ab3c4
gabrielleles18 <gabriel.leles18@gmail.com>
2021-01-25 23:42:14 -0300

    layout pedido

--- app/view/pedido/index.php
+++ app/view/pedido/index.php
@@ -24,8 +24,27 @@ $breadcrumb = Index::gerateBreadcrumb([
 <div class="container">
     <h1>Pedido</h1>
     <div class="box">
-        <?php echo "<pre>";
-        print_r($pedido); ?>
+        <div class="busy">
+            <img src="<?= URL . '/public/img/table.png' ?>"/>
+            <span>
+                <h4>Mesa <?= $mesa->numero ?></h4>
+                <h5>Mesa <?= $mesa->descricao ?></h5>
+            </span>
+            <?php
+            if ($pedido->status == 1)
+                $status = 'Aberto';
+            elseif ($pedido->status == 0)
+                $status = 'Fechado';
+            else
+                $status = 'Em andamento';
+            ?>
+        </div>
+        <ul class="pedido-item">
+            <li>Status <span><?= $status ?></span></li>
+            <li>Detalhes <span><?= $pedido->observacoes ?></span></li>
+            <li>Data/Hora <span><?= $pedido->data_pedido ?></span></li>
+            <li>Valor Total <span><?= $pedido->valor ?></span></li>
+        </ul>
     </div>
 </div>
 </div>
