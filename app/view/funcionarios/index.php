9ace4778aef61b41837c542a5e3ab8972eefbad8
gabrielleles18 <gabriel.leles18@gmail.com>
2021-02-02 23:59:09 -0300

    bug

--- app/view/funcionarios/index.php
+++ app/view/funcionarios/index.php
@@ -17,17 +17,45 @@ $breadcrumb = Index::gerateBreadcrumb([
 <div class="container">
     <h1>Funcionarios</h1>
     <div class="box">
+<!--        <h3>Total de funcioanrios: --><?//= $amount_of_funcionarios; ?><!--</h3>-->
+        <table>
+            <thead>
+            <tr>
+                <td>Nome</td>
+                <td>CPF</td>
+                <td>Telefone</td>
+                <td>Função</td>
+                <td>Status</td>
+                <td>Excluir</td>
+                <td>Editar</td>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($funcionarios as $funcionario) { ?>
+                <tr>
+                    <td><?php if (isset($funcionario->nome)) echo $funcionario->nome; ?></td>
+                    <td><?php if (isset($funcionario->cpf)) echo $funcionario->cpf; ?></td>
+                    <td><?php if (isset($funcionario->telefone)) echo $funcionario->telefone; ?></td>
+                    <td><?php if (isset($funcionario->funcao)) echo $funcionario->funcao; ?></td>
+                    <td><?php if (isset($funcionario->status)) echo $funcionario->status; ?></td>
+                    <td>
+                        <a href="<?= URL . 'funcionarios/delete/' . $funcionario->idfuncionario; ?>"><i class="far fa-trash-alt"></i></a>
+                    </td>
+                    <td>
+                        <a href="<?= URL . 'funcionarios/edit/' . $funcionario->idfuncionario; ?>">Editar</a>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="box">
         <form action="<?= URL; ?>funcionarios/add" method="POST" class="form">
             <div class="form-row">
                 <div class="col-3 col-in">
                     <label>Nome</label>
-                    <div class="input-group mb-2 mr-sm-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="bi bi-person-fill"></i></div>
-                        </div>
-                        <input type="text" name="nome" value="" required class="form-control"
-                               id="inlineFormInputGroupUsername2" placeholder="Username">
-                    </div>
+                    <input type="text" name="nome" value="" required class="form-control"/>
                 </div>
                 <div class="col-3 col-in">
                     <label>CPF</label>
@@ -81,39 +109,6 @@ $breadcrumb = Index::gerateBreadcrumb([
                 </div>
             </div>
         </form>
-    </div>
-    <div class="box">
-        <h3>Total de funcioanrios: <?= $amount_of_funcionarios; ?></h3>
-        <table>
-            <thead>
-            <tr>
-                <td>Nome</td>
-                <td>CPF</td>
-                <td>Telefone</td>
-                <td>Função</td>
-                <td>Status</td>
-                <td>Excluir</td>
-                <td>Editar</td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($funcionarios as $funcionario) { ?>
-                <tr>
-                    <td><?php if (isset($funcionario->nome)) echo $funcionario->nome; ?></td>
-                    <td><?php if (isset($funcionario->cpf)) echo $funcionario->cpf; ?></td>
-                    <td><?php if (isset($funcionario->telefone)) echo $funcionario->telefone; ?></td>
-                    <td><?php if (isset($funcionario->funcao)) echo $funcionario->funcao; ?></td>
-                    <td><?php if (isset($funcionario->status)) echo $funcionario->status; ?></td>
-                    <td>
-                        <a href="<?= URL . 'funcionarios/delete/' . $funcionario->idfuncionario; ?>">Excluir</a>
-                    </td>
-                    <td>
-                        <a href="<?= URL . 'funcionarios/edit/' . $funcionario->idfuncionario; ?>">Editar</a>
-                    </td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
     </div>
 </div>
 </div>
